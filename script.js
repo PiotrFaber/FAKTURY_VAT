ebc1889f38a7bb4087032da541e5da19ebdfc59e
Piotr Faber <lebik76@gmail.com>
2023-12-05 17:35:34 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function dropdown_btn_Click() {
+let dropdownList = document.getElementById("dropdown-container");
+
+if (dropdownList.style.display !="none") {  
+        dropdownList.style.display ="none";  
+    } else {  
+        dropdownList.style.display ="block";  
+    }   
+ }  
